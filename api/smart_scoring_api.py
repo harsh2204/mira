c2835a553db1916b793b3c2cd5eed059b2832e8f
Harsh Gupta <harshgupta2204@live.com>
2019-03-11 15:42:28 -0400

    Added requirements.txt

--- api/smart_scoring_api.py
+++ api/smart_scoring_api.py
@@ -1,9 +1,13 @@
 import cherrypy
 import logging
+from textblob import TextBlob
 
 class Score(object):
-    def get_score(review):
-        # review object = [usefulness, user_rating, text]
-        
-        pass
+    def sentiment(self, string):
+        analysis = TextBlob(string)
+        return analysis.sentiment.polarity
+    def get_score(self, review):
+        # review object = [usefulness (), user_rating (x/10), text(-1, 1)]
+        score = review['use'] + review['rating'] + sentiment(review['text'])
+        return score
     pass
