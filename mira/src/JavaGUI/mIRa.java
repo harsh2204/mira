3a9cb6c93d3f1a44a8eb98fcd983c2aa5be1d7e9
Pandher <pandha1@mcmaster.ca>
2019-03-20 01:58:09 -0400

    Search bar works when enter is pressed, data needs to be created:

--- mira/src/JavaGUI/mIRa.java
+++ mira/src/JavaGUI/mIRa.java
@@ -3,6 +3,7 @@ package JavaGUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -14,6 +15,8 @@ import javax.swing.JMenuBar;
 import javax.swing.JLayeredPane;
 import java.awt.Panel;
 import javax.swing.JTabbedPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class mIRa {
 
@@ -54,14 +57,23 @@ public class mIRa {
 		frame.setBounds(150, 150, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.setBackground(Color.red);
+		frame.setBackground(Color.gray);
 		
 		textField = new JTextField();
+		textField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JLabel label = new JLabel("Top 3 Drugs:");
+				panel.add(label);
+				frame.setVisible(true);
+				
+				
+			}
+		});
 		textField.setBounds(155, 6, 119, 26);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		JButton btnSubmitQuery = new JButton("Submit Query");
+		JButton btnSubmitQuery = new JButton("submit");
 		btnSubmitQuery.setBounds(157, 34, 117, 29);
 		frame.getContentPane().add(btnSubmitQuery);
 		
@@ -70,10 +82,42 @@ public class mIRa {
 		frame.getContentPane().add(tabbedPane);
 		
 		panel = new JPanel();
-		tabbedPane.addTab("New tab", null, panel, null);
+		tabbedPane.addTab("drugs", null, panel, null);
 		
 		tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.addTab("New tab", null, tabbedPane_1, null);
+		tabbedPane.addTab("reviews", null, tabbedPane_1, null);
 		
 	}
 }
+
+
+
+
+
+
+/* TEST CODE FOR GUI */
+/* ................. */
+/* ................. */
+/* ................. */
+/* ................. */
+/* ................. */
+/* ................. */
+/* ................. */
+/* ................. */
+/* TEST CODE FOR GUI */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
