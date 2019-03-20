55af268e36804ec7d060a2d584ba393b72ebf8ec
Navroop Singh <singhn35@mcmaster.ca>
2019-03-20 05:53:23 -0400

    added design elements to the gui

--- mira/src/JavaGUI/mIRa.java
+++ mira/src/JavaGUI/mIRa.java
@@ -15,16 +15,26 @@ import javax.swing.JMenuBar;
 import javax.swing.JLayeredPane;
 import java.awt.Panel;
 import javax.swing.JTabbedPane;
+<<<<<<< HEAD
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+=======
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+>>>>>>> 77dd795d00dbca8e28a0fd95dc0eaeddcfd9c1c6
 
 public class mIRa {
 
 	private JFrame frame;
-	private JTextField textField;
+	private JTextField txtEnterTheDrugs;
 	private JTabbedPane tabbedPane;
 	private JPanel panel;
 	private JTabbedPane tabbedPane_1;
+	private JLabel lblNewLabel;
 
 	/**
 	 * Launch the application.
@@ -54,11 +64,33 @@ public class mIRa {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(150, 150, 450, 300);
+		frame.getContentPane().setBackground(new Color(204, 153, 153));
+		frame.getContentPane().setForeground(new Color(0, 0, 0));
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(mIRa.class.getResource("/JavaGUI/resources/pill.png")));
+		frame.setForeground(new Color(0, 0, 0));
+		frame.setBounds(150, 150, 759, 617);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setBackground(Color.gray);
 		
+<<<<<<< HEAD
+		txtEnterTheDrugs = new JTextField();
+		txtEnterTheDrugs.setFont(new Font("Microsoft YaHei Light", Font.PLAIN, 16));
+		txtEnterTheDrugs.setText("enter the drug's name here");
+		txtEnterTheDrugs.setForeground(new Color(211, 211, 211));
+		txtEnterTheDrugs.setBounds(15, 85, 707, 40);
+		frame.getContentPane().add(txtEnterTheDrugs);
+		txtEnterTheDrugs.setColumns(10);
+		
+		JButton btnSubmitQuery = new JButton("submit");
+		btnSubmitQuery.setFont(new Font("Microsoft YaHei Light", Font.PLAIN, 18));
+		btnSubmitQuery.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnSubmitQuery.setForeground(new Color(0, 0, 0));
+		btnSubmitQuery.setBounds(279, 141, 180, 29);
+=======
 		textField = new JTextField();
 		textField.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -75,17 +107,32 @@ public class mIRa {
 		
 		JButton btnSubmitQuery = new JButton("submit");
 		btnSubmitQuery.setBounds(157, 34, 117, 29);
+>>>>>>> 77dd795d00dbca8e28a0fd95dc0eaeddcfd9c1c6
 		frame.getContentPane().add(btnSubmitQuery);
 		
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(27, 64, 391, 195);
+		tabbedPane.setFont(new Font("Microsoft YaHei Light", Font.PLAIN, 16));
+		tabbedPane.setBounds(15, 180, 707, 365);
 		frame.getContentPane().add(tabbedPane);
 		
+		tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.addTab("drug info", null, tabbedPane_1, null);
+		
 		panel = new JPanel();
+<<<<<<< HEAD
+		panel.setBackground(new Color(248, 248, 255));
+		tabbedPane.addTab("reviews", null, panel, null);
+		
+		lblNewLabel = new JLabel("New label");
+		lblNewLabel.setIcon(new ImageIcon(mIRa.class.getResource("/JavaGUI/resources/logo2.PNG")));
+		lblNewLabel.setBounds(279, 0, 180, 69);
+		frame.getContentPane().add(lblNewLabel);
+=======
 		tabbedPane.addTab("drugs", null, panel, null);
 		
 		tabbedPane_1 = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.addTab("reviews", null, tabbedPane_1, null);
+>>>>>>> 77dd795d00dbca8e28a0fd95dc0eaeddcfd9c1c6
 		
 	}
 }
